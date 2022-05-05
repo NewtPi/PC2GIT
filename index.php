7c0711ff77563087b8125626b3dcc6353051c327
Newton <newton.pinedo@utec.edu.pe>
2022-05-04 19:30:34 -0500

    a

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
     <input type="text" name="nombre" placeholder="Ingrese el nombre completo">;
     <break>
     <input type="text" name="celular" placeholder="Ingrese el numero de celular">;
-    <br>
+    <break>
     <button type="submit" name="submit">Guardar contacto</button>
+    <break>
 
 </form>
 

2433472dcccd466b9410d854e86cca343b80d4cc
Newton <newton.pinedo@utec.edu.pe>
2022-05-04 21:10:15 -0500

    a

--- abrirSQL.php
+++ abrirSQL.php
@@ -4,10 +4,11 @@ $conn=new mysqli('us-cdbr-east-05.cleardb.net','bbe40a4158d781','bad350f8','hero
 $sql="SELECT nombre, celular FROM agendadb";
 $result=mysqli_query($conn, $sql);
 
+echo "Nombre: " . "  " . " - Celular: " ." ". "<br>";
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo "Nombre: " . $row["nombre"]. " - Celular: " . $row["celular"]. " ". "<br>";
+      echo $row["nombre"]. "  " . $row["celular"]. " ". "<br>";
     }
   } else {
     echo "0 results";
@@ -23,45 +24,3 @@ if ($result->num_rows > 0) {
     <button type="submit" name="regresar">Regresar a añadir contactos</button>
 
 </form>
-
-//$sql= "Select * from agendadb";
-//$result=mysqli_query($conn,$sql);
-//if ($result) {
-//    $row=mysqli_fetch_assoc($result);
-//    echo row['nombre'];
-//    $row=mysqli_fetch_assoc($result);
-//    echo row['celular'];
-//}
-?>
-
-<table class="table">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-            </tr>
-        </tbody>
-        </table>
-
-        

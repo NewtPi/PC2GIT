442dcc80675c52e3d77f21e18ecc7db610364471
Newton <newton.pinedo@utec.edu.pe>
2022-05-04 21:33:23 -0500

    a

--- abrirSQL.php
+++ abrirSQL.php
@@ -7,8 +7,25 @@
 <?php
 $conn=new mysqli('us-cdbr-east-05.cleardb.net','bbe40a4158d781','bad350f8','heroku_05ba08bf857ea97');
 
-$sql="SELECT nombre, celular FROM agendadb";
+$sql="SELECT ID, nombre, celular FROM agendadb";
 $result=mysqli_query($conn, $sql);
+
+if ($result) {
+    while ($row=mysqli_fetch_assoc($result)) {
+        $ID=$row['ID'];
+        $nombre=$row['nombre'];
+        $celular=$row['celular'];
+        echo ' <tr>
+        <th scope="row">'.$ID.'</th>
+        <td>'.$nombre.'</td>
+        <td>'.$celular.'</td>
+      </tr>';
+
+
+        }
+}
+
+
 
 echo "Nombre: " . " " ." " ." " ." " ." " . "Celular: " ." ". "<br>";
 if ($result->num_rows > 0) {

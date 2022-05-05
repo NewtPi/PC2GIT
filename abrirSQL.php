048d4db3ab2fc85bcced31c0705e672e4a663793
Newton <newton.pinedo@utec.edu.pe>
2022-05-04 21:05:27 -0500

    a

--- abrirSQL.php
+++ abrirSQL.php
@@ -1,11 +1,29 @@
 <?php
 $conn=new mysqli('us-cdbr-east-05.cleardb.net','bbe40a4158d781','bad350f8','heroku_05ba08bf857ea97');
+
+$sql="SELECT nombre, celular FROM agendadb";
+$result=mysqli_query($conn, $sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "Nombre: " . $row["nombre"]. " - Celular: " . $row["celular"]. " ". "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+
 ?>
+
+
+
 <form action="index.php" method="POST">
 
     <button type="submit" name="regresar">Regresar a añadir contactos</button>
 
 </form>
+
 //$sql= "Select * from agendadb";
 //$result=mysqli_query($conn,$sql);
 //if ($result) {

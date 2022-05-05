be2270fa27e2f51e2a04b2be010fd385845d646e
Newton <newton.pinedo@utec.edu.pe>
2022-05-04 19:53:20 -0500

    a

--- connect.php
+++ connect.php
@@ -1,16 +1,15 @@
 <?php
-$con=new mysqli('us-cdbr-east-05.cleardb.net','bbe40a4158d781','bad350f8','heroku_05ba08bf857ea97');
 
+$conn=new mysqli('us-cdbr-east-05.cleardb.net','bbe40a4158d781','bad350f8','heroku_05ba08bf857ea97');
+header("Location: index.php?signup=sucess");
 $nombre=$_POST['nombre'];
 $celular=$_POST['celular'];
-
-
 
 $sql="INSERT INTO agendadb (nombre, celular) VALUES ('$nombre','$celular')";
 mysqli_query($conn, $sql);
 
 
-header("Location: index.php?signup=sucess");
+
 if($con){
     echo "SE CONECTOOOO";
 }

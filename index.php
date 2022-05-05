3707bdc12398eaf2104e5a93f00dfed04b7e332e
Newton <newton.pinedo@utec.edu.pe>
2022-05-04 20:44:27 -0500

    a

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 
 <!DOCTYPE html>
 <html>
@@ -19,41 +16,11 @@
 
 </form>
 
-<?php
+<form action="abrirSQL.php" method="POST">
 
+    <button type="submit" name="veragenda">Ver agenda</button>
 
-?>
-
-<table class="table">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-            </tr>
-        </tbody>
-        </table>
-
+</form>
 
 
 
